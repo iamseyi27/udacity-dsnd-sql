0349beb60a3ff25eb70710e7ea015d903e6d9eec
Idegbekwu-Oluwaseyi <idegbekwuseyi27@gmail.com>
2023-12-18 03:39:52 +0100

    Add L01_12.sql to the repo

---
+++ Basic_SQL/L01_12.sql
@@ -0,0 +1,4 @@
+-- Your first Queries in SQL 
+-- Try writing your own query to select only the id, account_id, and occurred_at columns for all orders in the orders table.
+SELECT order_id, account_id, occurred_at
+	FROM orders;
